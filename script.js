a3a0e38b2050d295b6ac4a515e51f65ebe29b2b5
Liam-Coulson <coulsonl2209@gmail.com>
2023-08-13 23:10:11 +0100

    Add basic JS password match validation

---
+++ script.js
@@ -0,0 +1,17 @@
+const passwordField = document.getElementById("password");
+const confirmPasswordField = document.getElementById("confirm_password");
+
+passwordField.addEventListener("input", () => checkMatch());
+confirmPasswordField.addEventListener("input", () => checkMatch());
+
+
+function checkMatch() {
+    if (passwordField.value != confirmPasswordField.value) {
+        confirmPasswordField.setCustomValidity("Passwords do not match");
+        console.log("Passwords do not match");
+        console.log("password is: ", passwordField.value);
+    } else {
+        confirmPasswordField.setCustomValidity("");
+        console.log("Passwords match");
+    }
+}
